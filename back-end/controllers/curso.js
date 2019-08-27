2be45b0ff651bc7c68fdd2e29f88915912d74a0c
Fausto G. Cintra <fausto@fgcintra.pro.br>
2019-08-27 20:31:18 -0300

    (27/08) Operaçoes CRUD (intervalo)

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -15,4 +15,53 @@ controller.novo = async function(req, res) {
    }
 }
 
+controller.listar = async function(req, res) {
+   try {
+      // find() sempre retorna um VETOR,
+      // mesmo que vazio
+      const cursos = await Curso.find();
+      res.send(cursos);
+   }
+   catch(erro) {
+      console.error(erro);
+      res.sendStatus(500).end();
+   }
+}
+
+controller.obterUm = async function(req, res) {
+   const id = req.params.id;
+   try {
+      const curso = await Curso.findById(id);
+      if(curso) {    // Curso encontrado (variável preenchida)
+         res.send(curso);
+      }
+      else {      // Curso não encontrado (variável vazia)
+         // HTTP 404: Not found
+         res.sendStatus(404).end();
+      }
+   }
+   catch(erro) {
+      console.error(erro);
+      res.sendStatus(500).end();
+   }
+}
+
+controller.atualizar = async function(req, res) {
+   const id = req.body._id;
+   try {
+      const curso = await Curso.findByIdAndUpdate(id, req.body);
+      if(curso) {
+         // HTTP 204: No content
+         res.sendStatus(204).end();
+      }
+      else {
+         res.sendStatus(404).end();
+      }
+   }
+   catch(erro) {
+      console.error(erro);
+      res.sendStatus(500).end();
+   }
+}
+
 module.exports = controller;
--- back-end/routes/curso.js
+++ back-end/routes/curso.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/curso');
 
 router.post('/', controller.novo);
+router.get('/', controller.listar);
+router.get('/:id', controller.obterUm);
+router.put('/', controller.atualizar);
 
 module.exports = router;
