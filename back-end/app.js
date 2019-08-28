eff5a2ada0ad49135e97b034ac91c7b894578878
Fausto G. Cintra <fausto@fgcintra.pro.br>
2019-08-27 22:15:36 -0300

    (27/08) CRUDS concluidos: curso e professor

--- back-end/app.js
+++ back-end/app.js
@@ -26,4 +26,7 @@ app.use('/teste', testeRouter);
 const curso = require('./routes/curso');
 app.use('/curso', curso);
 
+const professor = require('./routes/professor');
+app.use('/professor', professor);
+
 module.exports = app;
