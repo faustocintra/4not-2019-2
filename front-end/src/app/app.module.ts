a58dbcba7a894cb54909eabbd9607d528181dbfc
Fausto G. Cintra <fausto@fgcintra.pro.br>
2019-10-11 11:14:44 -0300

    (11/10) Adiçao de cabeçalho, menu e rodape

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { AppHeaderComponent } from './ui/app-header/app-header.component';
+import { AppMenuComponent } from './ui/app-menu/app-menu.component';
+import { AppFooterComponent } from './ui/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
